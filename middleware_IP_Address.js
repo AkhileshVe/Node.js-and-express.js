5e1e0cc6187066c1a3c7264283bddb3d37fd8e0f
Akhilesh verma <Akhileshverma207@gmail.com>
2025-11-17 23:49:03 +0530

    middleware ip address example

---
+++ middleware_IP_Address.js
@@ -0,0 +1,38 @@
+import express from "express";
+import path from "path"
+const app = express();
+
+const abspath = path.resolve("pages")
+
+
+
+function ageCheck(req,resp,next){
+if(!req.query?.age || req.query.age<=18){
+resp.send("<h1>Alert ! you can not access this page</h1>")
+}else {
+    next();
+}}
+
+app.use(ageCheck)
+
+app.get("/",(req,resp)=>{
+resp.send("<h1>This is home page</h1>")
+})
+
+app.get("/login",(req,resp)=>{
+resp.send("<h1>This is login page</h1>")
+})
+
+app.get("/about",(req,resp)=>{
+resp.send("<h1>This is about page</h1>")
+})
+
+app.get("/contact",(req,resp)=>{
+resp.send("<h1>This is contact page</h1>")
+})
+
+app.use((req,resp)=>{
+resp.status(404).sendFile(abspath+"/404.html")
+})
+
+app.listen(3500)
